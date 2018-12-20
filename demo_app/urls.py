fd99814d35f0ca47bc6307638df293a5b26ab48c
atsushi833koshiba <fkrie36b@yahoo.co.jp>
2018-12-21 00:28:07 +0900

    design revise

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('menu',views.menu, name='menu'),
+    path('input_form',views.input_form, name='input_form'),
+    path('result',views.result, name='result')
+
 ]
